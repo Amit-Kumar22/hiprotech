bb76e6e3a9f315aa177d23c8f773c67a6479ff49
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2025-07-03 10:38:29 +0530

    add in  vercel

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,16 +1,20 @@
-const mongoose = require('mongoose');
+
+import mongoose from 'mongoose';
+
 
 const MONGODB_URI = process.env.MONGODB_URI;
+
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
 }
 
-let cached = global.mongoose;
 
+let cached = global.mongoose;
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
+
 
 async function dbConnect() {
   if (cached.conn) {
@@ -28,4 +32,4 @@ async function dbConnect() {
   return cached.conn;
 }
 
-module.exports = dbConnect;
+export default dbConnect;
